020379738642d06e42aac4573e56f078a09aa7a8
Mykhailo Kovka <m.kovka@hls.tv>
2017-09-11 19:59:15 +0300

    's'

--- src/store/configureStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import videoApp from '../reducers/index'
-import thunkMiddleware from 'redux-thunk'
-export default function createAppStore(initialState) {
-    const  store = createStore(videoApp,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),applyMiddleware(thunkMiddleware), initialState);
-    return store
-}
